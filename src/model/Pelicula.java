ffcd69f2168759fb9e33347c969e6e9fd961cdd7
Julio Oyarzun <juliooyarzun2024@gmail.com>
2026-04-14 18:38:25 -0400

    feat(model): add Pelicula class with private attributes and getters

--- src/model/Pelicula.java
+++ src/model/Pelicula.java
@@ -1,4 +1,22 @@
 package model;
 
 public class Pelicula {
+    private String titulo;
+    private int duracion;
+
+    public Pelicula(String titulo, int duracion) {
+        this.titulo = titulo;
+        this.duracion = duracion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+    public void mostrarInfo() {
+        System.out.println("Peliculas " + titulo + "Duracion" + duracion + "Min");
+    }
 }
