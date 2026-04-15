bef35f83074e9204bddbb22a270271ff766f3b32
Angela Galleguillos <angie.galleguillez@gmail.com>
2026-04-14 21:23:42 -0400

    Feature/angela galleguillos (#8)
    * feat(CineService): add class structure (#1)
    
    - added constructor and getters/setters
    - added structure of other functions
    
    * update dev (#4)
    
    * feat(CineService): add class structure (#1) (#2)
    
    - added constructor and getters/setters
    - added structure of other functions
    
    Co-authored-by: María C. Riquelme <44560427+raiksha@users.noreply.github.com>
    
    * feat: add CONTRIBUTING.md file
    
    * docs(CONTRIBUTING): update CONTRIBUTING.md
    
    ---------
    
    Co-authored-by: María C. Riquelme <44560427+raiksha@users.noreply.github.com>
    
    * Feature/diego castillo (#3)
    
    * feat(CineService): add class structure (#1) (#2)
    
    - added constructor and getters/setters
    - added structure of other functions
    
    Co-authored-by: María C. Riquelme <44560427+raiksha@users.noreply.github.com>
    
    * feat(service): implement do-while menu with input validation in CineService
    - Added mostrarMenuService() with styled console UI
    - Implemented do-while loop for menu navigation
    - Integrated switch-case for user options
    - Added exit condition to option 0
    
    ---------
    
    Co-authored-by: Pablo Fuentes Soto <pablodesk1024@gmail.com>
    Co-authored-by: María C. Riquelme <44560427+raiksha@users.noreply.github.com>
    
    * feat (Cliente & Main): complete project logic in Cliente and function added to Main.
    
    ---------
    
    Co-authored-by: María C. Riquelme <44560427+raiksha@users.noreply.github.com>
    Co-authored-by: Pablo Fuentes Soto <pablodesk1024@gmail.com>
    Co-authored-by: Diego Castillo <dstefanocastillo@gmail.com>

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,7 +2,8 @@ package app;
 
 import model.*;
 import service.*;
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -10,8 +11,49 @@ public class Main {
     private static CineService servicio = new CineService();
     private static Scanner scanner = new Scanner(System.in);
 
+    static List<Cliente> listaClientes = new ArrayList<>();
+
     public static void main(String[] args) {
         servicio.mostrarMenuService();
         scanner.close();
+    }
+
+    public static void crearCliente() {
+
+        System.out.println("--- REGISTRAR NUEVO CLIENTE ---");
+
+        System.out.print("Nombre del cliente: ");
+        String nombre = scanner.nextLine().trim();
+
+        if (nombre.isEmpty()) {
+            System.out.println("ERROR: El nombre no puede estar vacío.");
+            return;
+        }
+
+        for (Cliente c : listaClientes) {
+            if (c.getNombre().equalsIgnoreCase(nombre)) {
+                System.out.println("ERROR: El cliente '" + nombre + "' ya está registrado.");
+                return;
+            }
+        }
+
+        System.out.print("Edad del cliente: ");
+        int edad;
+        try {
+            edad = Integer.parseInt(scanner.nextLine().trim());
+            if (edad < 0 ) {
+                System.out.println("ERROR: Ingrese una edad válida.");
+                return;
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("ERROR: La edad debe ser un número.");
+            return;
+        }
+
+        Cliente nuevoCliente = new Cliente(nombre, edad);
+
+        listaClientes.add(nuevoCliente);
+
+        System.out.println("\n¡LISTO! Cliente " + nombre + " creado correctamente.");
     }
 }
